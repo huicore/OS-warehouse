d8f1eadcc39962794e8fc964be906c103ba5047e
huicore <radi4ana@gmail.com>
2025-03-31 16:02:18 +1000

    Add files via upload

---
+++ AssetManagementSystem/Models/AuditLog.cs
@@ -0,0 +1,9 @@
+﻿public class AuditLog
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public string Action { get; set; } // "Удаление ОС", "Изменение пароля"
+    public string Details { get; set; } // JSON с изменениями
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+}
---
+++ AssetManagementSystem/Models/Role.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssetManagementSystem.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        // Права доступа
+        public bool CanManageAssets { get; set; }
+        public bool CanManageUsers { get; set; }
+        public bool CanViewReports { get; set; }
+        public bool CanExportData { get; set; }
+        public bool CanViewAuditLogs { get; set; }
+
+        // Связи
+        public ICollection<User> Users { get; set; }
+    }
+}

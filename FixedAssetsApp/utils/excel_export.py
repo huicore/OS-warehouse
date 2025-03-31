bc104f322b426a9cea19f4eb1bd8f58c61560d1b
huicore <radi4ana@gmail.com>
2025-03-31 17:41:52 +1000

    Delete FixedAssetsApp directory

--- FixedAssetsApp/utils/excel_export.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from openpyxl import Workbook
-
-def export_to_excel(data, filename):
-    df = pd.DataFrame(data)
-    writer = pd.ExcelWriter(filename, engine='openpyxl')
-    df.to_excel(writer, index=False)
-    writer.close()
